f772ef1ab55eab6a272aec239ce1ad7c2376c941
ouronghui <webdevelop@sohu.com>
2014-10-13 23:37:29 +0800

    jconfirm commit

---
+++ js-demo/my_jq_plugin/jconfirm/js/jconfirm.js
@@ -0,0 +1,15 @@
+/**
+ * @author orh
+ * 消息提示插件
+ */
+(function($) {
+	var empFun = $.noop,	//jquery空函数
+		defaults = {
+			content: '确定吗?',
+			sureBtnTex: '确认',
+			canBtnTex: '取消',
+			onSure: empFun,
+			onCancle: empFun
+		},
+		tipMain = -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;
+})(jQuery)
