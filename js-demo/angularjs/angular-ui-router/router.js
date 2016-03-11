7bbd704ec14f4c4b46bd7c9bf2dbef9bbe9028d4
orh <webdevelop@sohu.com>
2016-03-11 17:02:48 +0800

    angular-ui-router路由Demo

---
+++ js-demo/angularjs/angular-ui-router/router.js
@@ -0,0 +1,80 @@
+var app = angular.module('routerApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/index');
+
+	$stateProvider
+		.state('index', {
+			url: '/index',
+			views: {
+				'': {
+					templateUrl: 'tpls/index.html'
+				},
+				'topbar@index': {
+					templateUrl: 'tpls/topbar.html'
+				},
+				'main@index': {
+					templateUrl: 'tpls/home.html'
+				}
+			}
+		})
+		.state('index.usermng', {
+			url: '/usermng',
+			views: {
+				'main@index': {
+					templateUrl: 'tpls/usermng.html',
+					controller: function($scope, $state) {
+						$scope.addUserType = function() {
+							$state.go('index.usermng.addusertype');
+						}
+					}
+				},
+			}
+		})
+		.state('index.usermng.highendusers', {
+			url: '/highendusers',
+			templateUrl: 'tpls/highendusers.html'
+		})
+		.state('index.usermng.normalusers', {
+			url: '/normalusers',
+			templateUrl: 'tpls/normalusers.html'
+		})
+		.state('index.usermng.lowusers', {
+			url: '/lowusers',
+			templateUrl: 'tpls/lowusers.html'
+		})
+		.state('index.usermng.addusertype', {
+			url: '/addusertype',
+			templateUrl: 'tpls/addusertype.html',
+			controller: function($scope, $state) {
+				$scope.backToPrevious = function() {
+					widonw.history.back();
+				};
+			}
+		})
+		.state('index.permission', {
+			url: '/permission',
+			views: {
+				'main@index': {
+					template: '这里是权限管理'
+				}
+			}
+		})
+		.state('index.report', {
+			url: '/report',
+			views: {
+				'main@index': {
+					template: '这里是报表管理'
+				}
+			}
+		})
+		.state('index.settings', {
+			url: '/settings',
+			views: {
+				'main@index': {
+					template: '这里是系统设置'
+				}
+			}
+		});
+});
+
