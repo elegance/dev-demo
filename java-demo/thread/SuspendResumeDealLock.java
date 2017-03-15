8d6444e2e3e8fae3770d564061a8b9990271a215
orh <webdevelop@sohu.com>
2017-03-15 16:31:36 +0800

    java多线程编程相关演示 例子

---
+++ java-demo/thread/SuspendResumeDealLock.java
@@ -0,0 +1,32 @@
+public class SuspendResumeDealLock {
+
+    static class SynchronizedObject {
+        synchronized public void printString() {
+            System.out.println("begin");
+
+            if (Thread.currentThread().getName().equals("a")) {
+                System.out.println("a 线程永远suspend了");
+                Thread.currentThread().suspend();
+            }
+            System.out.println("end");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final SynchronizedObject object = new SynchronizedObject();
+
+        Thread thread1 = new Thread(() -> {
+            object.printString();
+        });
+        thread1.setName("a");
+        thread1.start();
+        Thread.sleep(1000);
+
+        Thread thread2 = new Thread(() -> {
+            System.out.println("thread2 启动了，但进入不了printString() 方法！只有 a 线程了打印了1个begin");
+            System.out.println("因为pringString()方法被 a 线程锁定并且永远suspend暂停了！");
+            object.printString();
+        });
+        thread2.start();
+    }
+}
