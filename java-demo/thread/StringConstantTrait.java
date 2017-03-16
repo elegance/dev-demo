b265e091144ef55b3b4098d8fa5dcd6a44e8e0a2
orh <webdevelop@sohu.com>
2017-03-16 17:10:50 +0800

    java多线程相关演示 例子

---
+++ java-demo/thread/StringConstantTrait.java
@@ -0,0 +1,36 @@
+public class StringConstantTrait {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "a";
+        
+        System.out.println(a == b); // true，引用相同
+        
+        Thread threadA = new Thread(() -> {
+            String lockStr = "a";
+            Service.pirnt(lockStr);
+        }, "thread-A");
+
+        Thread threadB = new Thread(() -> {
+            String lockStr2 = "a";
+            Service.pirnt(lockStr2); // lockStr2 其实还是指向常量池中的“a”,所以会被线程A阻塞，与期望不配
+        }, "thread-B");
+        
+        threadA.start();
+        threadB.start();
+    }
+    
+    static class Service {
+        public static void pirnt(String param) {
+            try {
+                synchronized(param) {
+                    while(true) {
+                        System.out.println(Thread.currentThread().getName());
+                        Thread.sleep(1000);
+                    }
+                }
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ java-demo/thread/SyncNotExtends.java
@@ -0,0 +1,45 @@
+public class SyncNotExtends {
+    
+    public static void main(String[] args) {
+        SubService service = new SubService();
+        
+        Thread threadA = new Thread(() -> {
+            service.serviceMethod();
+        }, "threadA☆");
+        
+        Thread threadB = new Thread(() -> {
+            service.serviceMethod();
+        }, "threadB★");
+        
+        threadA.start();
+        threadB.start();
+    }
+
+    static class BaseService {
+        synchronized public void serviceMethod() {
+            try {
+                System.out.printf("BaseService serviceMethod，下一步 sleep begin threadName=%s time=%d\n", Thread.currentThread().getName(), System.currentTimeMillis());
+                Thread.sleep(2000);
+                System.out.printf("BaseService serviceMethod，下一步 sleep end threadName=%s time=%d\n", Thread.currentThread().getName(), System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    static class SubService extends BaseService {
+        
+        @Override // 重写时去掉了 synchronized 标识
+        public void serviceMethod() {
+            try {
+                System.out.printf("SubService serviceMethod，下一步 sleep begin threadName=%s time=%d\n", Thread.currentThread().getName(), System.currentTimeMillis());
+                Thread.sleep(2000);
+                System.out.printf("SubService serviceMethod，下一步 sleep end threadName=%s time=%d\n", Thread.currentThread().getName(), System.currentTimeMillis());
+                
+                super.serviceMethod();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
