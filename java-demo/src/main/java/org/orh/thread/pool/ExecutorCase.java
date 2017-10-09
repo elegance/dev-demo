f22bcc477a41fdc3da5ceac61afa25795b6ddf25
elegance <webdevelop@sohu.com>
2017-10-09 09:24:38 +0800

    Excutors demo更新

--- java-demo/src/main/java/org/orh/thread/pool/ExecutorCase.java
+++ java-demo/src/main/java/org/orh/thread/pool/ExecutorCase.java
@@ -1,6 +1,5 @@
 package org.orh.thread.pool;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,21 +8,25 @@ public class ExecutorCase {
     private static ExecutorService executor = Executors.newFixedThreadPool(10);
 
     public static void main(String[] args) {
-        
         for (int i = 0; i < 20; i++) {
             executor.execute(new Task()); // execute 不会被阻塞
             System.out.println("submit: " + i); // 相继输出
         }
+        // 全部任务执行完，main方法不会结束，因为线程池中的线程只是结束了工作 进入了 idle 状态
     }
 
     static class Task implements Runnable {
         public void run() {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            sleep(5000); // 模拟异步查询数据库，睡眠5s
             System.out.println(Thread.currentThread().getName());
+        }
+    }
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
