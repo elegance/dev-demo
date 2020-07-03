12c31b5d1fde2e0772b88f7232a217d7e472ba25
ouronghui <ouronghui@moretickets.com>
2020-07-03 16:02:39 +0800

    rocksDB IterateUpperBound 测试

--- java-demo/src/main/java/org/orh/rocksdb/RocksDBSeekTest.java
+++ java-demo/src/main/java/org/orh/rocksdb/RocksDBSeekTest.java
@@ -8,10 +8,11 @@ import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * seek 测试
+ *
  * @author ouronghui
  * @since 2020/6/30
  */
@@ -21,25 +22,27 @@ public class RocksDBSeekTest {
 
     static ColumnFamilyHandle columnFamilyHandler;
 
-    static WriteOptions WRITE_OPTIONS_SYNC;
+    static WriteOptions writeOptions;
 
     static final String[] testKeys = {"10", "11", "12", "22", "21", "20", "c", "b", "a", "A", "B", "C", "0"};
 
     @BeforeClass
     public static void setup() throws RocksDBException {
-        org.rocksdb.Options options = new org.rocksdb.Options();
-        options.setCreateIfMissing(true);
+        Options options = new Options();
+        options
+                .setCreateIfMissing(true)
+                .setCompressionType(CompressionType.NO_COMPRESSION);
         rocksDB = RocksDB.open(options, "c:/Temp/rocks/seek-test");
         // columnFamilyHandler 列族， 类似于 表， 这里使用的默认"表"
         columnFamilyHandler = rocksDB.getDefaultColumnFamily();
-        WRITE_OPTIONS_SYNC = new WriteOptions();
-        WRITE_OPTIONS_SYNC.setSync(true);
+        writeOptions = new WriteOptions();
+        writeOptions.setSync(true);
 
         rocksDB.deleteRange("0".getBytes(), "z".getBytes());
 
         // 插入测试数据
         for (int i = 0; i < testKeys.length; i++) {
-            rocksDB.put(columnFamilyHandler, WRITE_OPTIONS_SYNC, testKeys[i].getBytes(), "none".getBytes());
+            rocksDB.put(columnFamilyHandler, writeOptions, testKeys[i].getBytes(), "none".getBytes());
         }
     }
 
@@ -79,4 +82,23 @@ public class RocksDBSeekTest {
         }
     }
 
+    @Test
+    public void testIterateUpperBound() {
+        // 目标数据：{"0", "10", "11", "12", "20", "21", "22", "A", "B", "C", "a", "b", "c"};
+        ReadOptions readOptions = new ReadOptions();
+        // 22 作为上界， 不包含22 ,即结果为6个数："0", "10", "11", "12", "20", "21"
+        readOptions.setIterateUpperBound(new Slice("22"));
+
+        RocksIterator iter = rocksDB.newIterator(columnFamilyHandler, readOptions);
+        iter.seekToFirst();
+        int cnt = 0;
+        while (iter.isValid()) {
+            String key = new String(iter.key(), StandardCharsets.UTF_8);
+            System.out.println(key);
+            assertThat(key, not(equalTo("22")));
+            iter.next();
+            cnt++;
+        }
+        assertThat(cnt, equalTo(6));
+    }
 }
